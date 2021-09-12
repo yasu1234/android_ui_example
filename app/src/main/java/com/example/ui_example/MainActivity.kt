65225a6acabf8801c1f8b1f914ad12338ae9f4fb
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2021-09-13 08:16:17 +0900

    add bottom navigation

--- app/src/main/java/com/example/ui_example/MainActivity.kt
+++ app/src/main/java/com/example/ui_example/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
