184b6fc3fa9d26043cc92fad7ea756e2834b873b
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2022-08-26 21:52:10 +0900

    remove AppBarConfiguration in BottomNavigationView

--- app/src/main/java/com/example/ui_example/maintab/HomeActivity.kt
+++ app/src/main/java/com/example/ui_example/maintab/HomeActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.ui_example.R
 
@@ -17,14 +15,6 @@ class HomeActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
