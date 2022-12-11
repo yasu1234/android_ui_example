6dabb6f96e80b83842089082d51428f0040b1f1c
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2022-12-11 21:12:28 +0900

    add edittext to show datePicker in spinner mode

--- app/src/main/java/com/example/ui_example/editText/EditTextActivity.kt
+++ app/src/main/java/com/example/ui_example/editText/EditTextActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ui_example.editText
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ui_example.R
+import kotlinx.android.synthetic.main.activity_edit_text.*
+import java.util.*
 
 class EditTextActivity: AppCompatActivity() {
 
@@ -10,5 +13,24 @@ class EditTextActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_edit_text)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        datePickerEditText.keyListener = null
+        datePickerEditText.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        val cal = Calendar.getInstance()
+
+        val datePickerDialog = DatePickerDialog(this, R.style.DatePickerDialog_Spinner, {_, year, month, dayOfMonth ->
+        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE))
+        datePickerDialog.show()
+        datePickerDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).setTextColor(this.getColor(R.color.colorAccent))
+        datePickerDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setTextColor(this.getColor(R.color.colorAccent))
     }
 }
