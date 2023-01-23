4b8f815b26854bedd0a3354acef620e73d0eb522
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-01-24 00:09:41 +0900

    add spannableString to replace word to image

--- app/src/main/java/com/example/ui_example/textView/SpannableStringActivity.kt
+++ app/src/main/java/com/example/ui_example/textView/SpannableStringActivity.kt
@@ -1,11 +1,14 @@
 package com.example.ui_example.textView
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
+import android.text.style.ImageSpan
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.ui_example.R
 import kotlinx.android.synthetic.main.activity_spannable_string.*
 
@@ -28,5 +31,15 @@ class SpannableStringActivity: AppCompatActivity() {
         )
 
         foregroundColorTextView.text = foregroundSpannable
+
+        val imageSpannable = SpannableStringBuilder("Sample Text")
+        val drawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.ic_home_black_24dp)
+        // to set lineHeight is responsible for various device
+        drawable?.setBounds(0, 0, spannableImageTextView.lineHeight, spannableImageTextView.lineHeight)
+
+        val span: ImageSpan? = drawable?.let { ImageSpan(it, ImageSpan.ALIGN_BASELINE) }
+        imageSpannable.setSpan(span, imageSpannable.length - 1, imageSpannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spannableImageTextView.text = imageSpannable
     }
 }
